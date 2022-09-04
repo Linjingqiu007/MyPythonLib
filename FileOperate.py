7eb13dbf695e8a305c9766faf8ea78ac1ca2b2ce
Linjingqiu007 <934210521@qq.com>
2022-09-04 15:48:32 +0800

    First time upload FileOperate.py

---
+++ FileOperate.py
@@ -0,0 +1,42 @@
+# Date 2022/09/04
+# Author Lin jingqiu
+# This Lib is for File Operation
+
+import os
+
+class fileOperation(object):
+
+    def __init__(self):
+        self.fileName = None
+        self.filePath = None
+
+    def newDir(self,path,name):
+        a = os.path.exists(os.path.join(path,name))
+        if a:
+            print("The folder is already exists")
+        else:
+            os.mkdir(os.path.join(path,name))
+            a = os.path.exists(os.path.join(path,name))
+            if a:
+                print("The folder built success")
+            else:
+                print("The folder built fail")
+
+    def delDir(self,path,name):
+        a = os.path.exists(os.path.join(path,name))
+        if a:
+            os.rmdir(os.path.join(path,name)) 
+        else:
+            print("The folder is not exists")
+        a = os.path.exists(os.path.join(path,name))
+        if a:
+            print("The folder deleted fail")
+        else:
+            print("The folder deleted success")
+
+
+if __name__ == "__main__":
+    myfile = fileOperation()
+    myfile.newDir("D:\git codebase\MyPythonLib","abc")
+    myfile.delDir("D:\git codebase\MyPythonLib","abc")
+    del myfile
